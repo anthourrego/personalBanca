dc8e7fb9c4f5b38dc312698a4cbe92c1c5668313
Anthony Smidh Urrego Pineda <35052057+anthourrego@users.noreply.github.com>
2021-11-17 22:21:04 -0500

    Update vNIT.php

--- vNIT.php
+++ vNIT.php
@@ -115,3 +115,23 @@
 		});
 	})();
 </script>
+
+function soloLetrasNumeros(e, input){
+    key = e.keyCode || e.which;
+    tecla = String.fromCharCode(key).toLowerCase();
+    letras = "abcdefghijklmnopqrstuvwxyz1234567890-+/";
+    especiales = "8-37-39-46";
+
+    tecla_especial = false;
+    
+    for (var i in especiales) {
+        if (key == especiales[i]) {
+            tecla_especial = true;
+            break;
+        }
+    }
+
+    if (letras.indexOf(tecla)==-1 && !tecla_especial) {
+        return false;
+    }
+}
